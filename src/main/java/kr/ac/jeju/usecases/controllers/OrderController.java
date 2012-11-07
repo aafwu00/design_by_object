fa0c2e4aa879e7cae7d01de6dc56eae755007080
김태호 <aafwu00@daumcorp.com>
2012-11-07 22:28:13 +0900

    17. Create Order 를 Web 통한 생성 테스트

--- src/main/java/kr/ac/jeju/usecases/controllers/OrderController.java
+++ src/main/java/kr/ac/jeju/usecases/controllers/OrderController.java
@@ -1,5 +1,36 @@
 package kr.ac.jeju.usecases.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import kr.ac.jeju.usecases.boundaries.BoundaryFactory;
+import kr.ac.jeju.usecases.boundaries.CreateOrderBoundary;
+import kr.ac.jeju.usecases.requestmodels.CreateOrderRequestModel;
+import kr.ac.jeju.usecases.requestmodels.CreateOrderRequestModelBuilder;
+
 public class OrderController {
+
+	public void create(final HttpServletRequest request, final HttpServletRequest response) {
+		final CreateOrderRequestModel requestModel = buildRequestModel(request);
+		final CreateOrderBoundary boundary = BoundaryFactory.createOrder(requestModel);
+		boundary.execute();
+		response.setAttribute("orderId", boundary.deliverOrderId());
+	}
+
+	private CreateOrderRequestModel buildRequestModel(final HttpServletRequest request) {
+		final Integer customerId = Integer.valueOf(request.getParameter("customerId"));
+		final String customerContactInfo = request.getParameter("customerContactInfo");
+		final String shipmentDestination = request.getParameter("shipmentDestination");
+		final String shipmentMechanism = request.getParameter("shipmentMechanism");
+		final String paymentInformation = request.getParameter("paymentInformation");
+
+		final CreateOrderRequestModelBuilder builder = new CreateOrderRequestModelBuilder();
+		builder.setCustomerId(customerId);
+		builder.setCustomerContactInfo(customerContactInfo);
+		builder.setShipmentDestination(shipmentDestination);
+		builder.setShipmentMechanism(shipmentMechanism);
+		builder.setPaymentInformation(paymentInformation);
+		final CreateOrderRequestModel requestModel = builder.build();
+		return requestModel;
+	}
 
 }
--- src/test/java/kr/ac/jeju/usecases/controllers/OrderControllerTest.java
+++ src/test/java/kr/ac/jeju/usecases/controllers/OrderControllerTest.java
@@ -1,12 +1,42 @@
 package kr.ac.jeju.usecases.controllers;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
 
 public class OrderControllerTest {
 
-	@Test
-	public void shouldBeCreateController() throws Exception {
-		final OrderController controller = new OrderController();
+	private OrderController controller;
+	private MockHttpServletRequest request;
+	private MockHttpServletRequest response;
+
+	private final String customerId = "1";
+	private final String customerContactInfo = "contactInfo";
+	private final String shipmentDestination = "destination";
+	private final String shipmentMechanism = "mechanism";
+	private final String paymentInformation = "paymentInformation";
+
+	@Before
+	public void setUp() throws Exception {
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletRequest();
+		controller = new OrderController();
 	}
 
+	@Test
+	public void shouldBeCreateOrder() throws Exception {
+		request.setParameter("customerId", customerId);
+		request.setParameter("customerContactInfo", customerContactInfo);
+		request.setParameter("shipmentDestination", shipmentDestination);
+		request.setParameter("shipmentMechanism", shipmentMechanism);
+		request.setParameter("paymentInformation", paymentInformation);
+
+		controller.create(request, response);
+
+		assertThat(response.getAttribute("orderId"), is(notNullValue()));
+	}
 }
