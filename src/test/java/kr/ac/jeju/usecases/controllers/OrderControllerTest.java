22c06d7c418c1ac6d3fd3547f10105f3cffc1cd6
김태호 <aafwu00@daumcorp.com>
2012-11-08 11:38:49 +0900

    17. Order Controller 생성

---
+++ src/test/java/kr/ac/jeju/usecases/controllers/OrderControllerTest.java
@@ -0,0 +1,11 @@
+package kr.ac.jeju.usecases.controllers;
+
+import org.junit.Test;
+
+public class OrderControllerTest {
+	@Test
+	public void shouldBeCreateController() throws Exception {
+		final OrderController controller = new OrderController();
+	}
+
+}
